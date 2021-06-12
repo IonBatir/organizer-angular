30cec32156ab35705416371d5795266ec85360a1
Ion Batîr <ionbatir@gmail.com>
2021-06-12 16:37:01 +0300

    Use dayjs for date service

--- src/app/services/date.service.ts
+++ src/app/services/date.service.ts
@@ -1,9 +1,13 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {BehaviorSubject} from "rxjs";
+import * as dayjs from "dayjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DateService {
+  public date: BehaviorSubject<dayjs.Dayjs> = new BehaviorSubject(dayjs());
 
-  constructor() { }
+  constructor() {
+  }
 }
