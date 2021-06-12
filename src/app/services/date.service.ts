0c70abea4bc8ec29fbc923d745ec220501fb83ab
Ion Batîr <ionbatir@gmail.com>
2021-06-12 18:03:33 +0300

    Set ro locale for dayjs

--- src/app/services/date.service.ts
+++ src/app/services/date.service.ts
@@ -1,12 +1,13 @@
 import {Injectable} from '@angular/core';
 import {BehaviorSubject} from "rxjs";
 import * as dayjs from "dayjs";
+import 'dayjs/locale/ro'
 
 @Injectable({
   providedIn: 'root'
 })
 export class DateService {
-  public date: BehaviorSubject<dayjs.Dayjs> = new BehaviorSubject(dayjs());
+  public date: BehaviorSubject<dayjs.Dayjs> = new BehaviorSubject(dayjs().locale('ro'));
 
   changeMonth(direction: number) {
     const value = this.date.value.add(direction, 'month');
