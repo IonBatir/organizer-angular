4b2fce39e75228ae9100325ad043633b1004309e
Ion Batîr <ionbatir@gmail.com>
2021-06-12 16:01:12 +0300

    Generate organizer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
+import { MonthSelectorComponent } from './components/month-selector/month-selector.component';
+import { OrganizerComponent } from './components/organizer/organizer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent,
+    MonthSelectorComponent,
+    OrganizerComponent
   ],
   imports: [
     BrowserModule
