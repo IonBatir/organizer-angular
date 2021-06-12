04e2019241c87572339f0d1bae5cc102855c62da
Ion Batîr <ionbatir@gmail.com>
2021-06-12 20:00:32 +0300

    Add auth with google

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import {AngularFireAuth} from "@angular/fire/auth";
+import firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +8,13 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(public auth: AngularFireAuth) {
+  }
+
+  login() {
+    this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
+  logout() {
+    this.auth.signOut();
+  }
 }
